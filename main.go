bd63b05a5389c3bd8343fc689c41965da69a6776
Duncan Leo <duncanleo97@gmail.com>
2015-10-02 12:17:03 +0800

    Mostly functional bot

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"fmt"
 	"strconv"
 	"github.com/ChimeraCoder/anaconda"
+	"time"
+	"encoding/json"
+	"io/ioutil"
 )
 
 type savedtweet struct {
@@ -16,24 +19,36 @@ type savedtweet struct {
 	tweetedDate string
 }
 
-const tweetsFileName string = "tweets.csv"
-
-const consumerKey = "5jAS8RyLoCFH1eZOZ6VZYA"
-const consumerSecret = "6UJAQl7WbBexqJXCvPaIOQxf1eMapr4CFqENSWpiSoA"
-const accessKey = "64090321-yFCT66IFZOGhm0qVE34CdhBpSBwo8EO4ftvK1Dfzz"
-const accessSecret = "LRrzFJXFvzJGvGSXXr3cPVpmeg78tcv5XueAVwnBpEs"
+type Config struct {
+	TweetsFileName string `json:"tweetsFileName"`
+	ConsumerKey string `json:"consumerKey"`
+	ConsumerSecret string `json:"consumerSecret"`
+	AccessKey string `json:"accessKey"`
+	AccessSecret string `json:"accessSecret"`
+}
 
 var oldestTweetId int64 = -1
 var params = url.Values{}
-var api = anaconda.NewTwitterApi(accessKey, accessSecret)
+var api *anaconda.TwitterApi
+var cfg Config
 
 func main() {
-	anaconda.SetConsumerKey(consumerKey)
-	anaconda.SetConsumerSecret(consumerSecret)
+	cfg_file, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Println("Error reading config.json")
+		return
+	}
 
-	if _, err := os.Stat(tweetsFileName); os.IsNotExist(err) {
-		fmt.Printf("no such file or directory: %s", tweetsFileName)
-		newFile, _ := os.Create(tweetsFileName)
+	json.Unmarshal(cfg_file, &cfg)
+	api = anaconda.NewTwitterApi(cfg.AccessKey, cfg.AccessSecret)
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	anaconda.SetConsumerKey(cfg.ConsumerKey)
+	anaconda.SetConsumerSecret(cfg.ConsumerSecret)
+
+	if _, err := os.Stat(cfg.TweetsFileName); os.IsNotExist(err) {
+		fmt.Printf("no such file or directory: %s", cfg.TweetsFileName)
+		newFile, _ := os.Create(cfg.TweetsFileName)
 		newFile.Close()
 	}
 
@@ -45,11 +60,10 @@ func main() {
 
 func initialCrawl() {
 	log.Println("Starting crawl...")
-	file, err := os.Open(tweetsFileName)
+	file, err := os.Open(cfg.TweetsFileName)
 	if err != nil {
 		log.Println("Error: ", err)
 	}
-	defer file.Close()
 
 	reader := csv.NewReader(file)
 	lines, err := reader.ReadAll()
@@ -57,74 +71,142 @@ func initialCrawl() {
 		log.Println("Error reading all lines: %v", err)
 	}
 
+	file.Close()
+
 	if len(lines) > 0 {
 		lastTweetIdString := lines[len(lines) - 1][0]
 
 		if s, err := strconv.ParseInt(lastTweetIdString, 10, 64); err == nil {
 			oldestTweetId = s
 		}
+		oldestTweetId, _ = strconv.ParseInt(lines[0][0], 10, 64)
 		log.Printf("Starting crawl from %d\n", oldestTweetId)
-	} else {
-		log.Println("Fresh Crawl")
-	}
 
-	crawlFrom(oldestTweetId)
-}
+		afterLastTimeTweets, err := multiCrawl(-1, oldestTweetId)
 
-func crawlFrom(tweetId int64) {
-	newTweets, err := api.GetUserTimeline(params)
-
-	if err != nil {
-		log.Println(err)
-	}
-
-	for len(newTweets) > 0 {
-		fmt.Printf("Getting tweets before %d\n", oldestTweetId)
-		if oldestTweetId != -1 {
-			// all subsequent requests use the max_id param to prevent duplicates
-			params.Set("max_id", strconv.FormatInt(oldestTweetId, 10))
-		}
-
-		newTweets, err = api.GetUserTimeline(params)
 		if err != nil {
 			log.Println(err)
 		}
 
-		file, err := os.Open(tweetsFileName)
+		saveCrawls(afterLastTimeTweets)
+
+		log.Printf("[SINCE LAST TIME CRAWL] %d tweets in total were retrieved\n", len(afterLastTimeTweets))
+
+	} else {
+		log.Println("Fresh Crawl")
+
+		beforeNowTweets, err := multiCrawl(-1, -1)
 		if err != nil {
-			log.Println("Error: ", err)
+			log.Println(err)
 		}
-		defer file.Close()
 
-		reader := csv.NewReader(file)
-		lines, err := reader.ReadAll()
+		saveCrawls(beforeNowTweets)
+
+		log.Printf("[INITIAL CRAWL] %d tweets in total were retrieved\n", len(beforeNowTweets))
+	}
+
+	//Interval crawl
+	t := time.NewTicker(time.Duration(5) * time.Minute)
+	for {
+		intervalTweets, err := multiCrawl(-1, oldestTweetId)
 		if err != nil {
-			log.Println("Error reading all lines: %v", err)
+			log.Println(err)
 		}
-		newTweetsStringArray := parseToString2dArray(newTweets)
-		lines = append(lines, newTweetsStringArray...)
-
-		writer := csv.NewWriter(file)
-		writer.WriteAll(lines)
-		if err := writer.Error(); err != nil {
-			log.Println("Error Writing CSV: ", err)
-		}
-
-		if s, err := strconv.ParseInt(lines[len(lines) - 1][0], 10, 64); err == nil {
-			oldestTweetId = s
-		}
-
-		log.Printf("%d tweets downloaded so far\n", len(lines))
+		saveCrawls(intervalTweets)
+		log.Printf("[INTERVAL CRAWL] %d tweets in total were retrieved\n", len(intervalTweets))
+		<- t.C
 	}
 }
 
-func parseToString2dArray(tweets []anaconda.Tweet) (tweetStringsArray [][]string) {
+//Crawl tweets with max_id or since_id until finished. Crawls 200 at a time and returns everything.
+func multiCrawl(max_id, since_id int64) ([][]string, error) {
+	allTweets := make([][]string, 0)
+	tweets, err := crawl(max_id, since_id)
+	if err != nil {
+		log.Println(err)
+		return [][]string{}, err
+	}
+
+	for len(tweets) > 1 {
+		allTweets = append(allTweets, tweets...)
+		if since_id != -1 {
+			oldestTweetId, _ = strconv.ParseInt(allTweets[0][0], 10, 64)
+			tweets, err = crawl(-1, oldestTweetId)
+		} else {
+			oldestTweetId, _ = strconv.ParseInt(allTweets[len(allTweets) - 1][0], 10, 64)
+			tweets, err = crawl(oldestTweetId, -1)
+		}
+		
+		if err != nil {
+ 			log.Println(err)
+ 			continue
+ 		}
+		log.Printf("[MULTI-CRAWLER] Progress: %d tweets retrieved\n", len(allTweets))
+	}
+
+	return allTweets, nil
+}
+
+//Crawls tweets with max_id or since_id. Max 200, according to Twitter's API.
+func crawl(max_id, since_id int64) ([][]string, error) {
+	if max_id != -1 {
+		params.Set("max_id", strconv.FormatInt(max_id, 10))
+	}
+	if since_id != -1 {
+		params.Set("since_id", strconv.FormatInt(since_id, 10))
+	}
+
+	tweets, err := api.GetUserTimeline(params)
+
+	if err != nil {
+		log.Println(err)
+		return [][]string{}, err
+	}
+
+	return parseToString2dArray(tweets), nil	
+}
+
+//Saves new tweets. Appends to existing tweets.
+func saveCrawls(tweets [][]string) {
+	file, err := os.Open(cfg.TweetsFileName)
+	if err != nil {
+		log.Println("Error: ", err)
+		return
+	}
+
+	reader := csv.NewReader(file)
+	lines, err := reader.ReadAll()
+	if err != nil {
+		log.Println("Error reading all lines: %v", err)
+	}
+
+	file.Close()
+
+	lines = append(lines, tweets...)
+
+	file, err = os.Create(cfg.TweetsFileName)
+	if err != nil {
+		log.Println("Error: ", err)
+		return
+	}
+
+	// withHeader := make([][]string, len(lines) + 1)
+	// withHeader = append(withHeader, []string{"id", "text", "createdAt"})
+	// withHeader = append(withHeader, lines...)
+
+	writer := csv.NewWriter(file)
+	writer.WriteAll(lines)
+	if err := writer.Error(); err != nil {
+		log.Println("Error Writing CSV: ", err)
+	}
+	file.Close()
+}
+
+//Parses anaconda library tweets into a 2d array
+func parseToString2dArray(tweets []anaconda.Tweet) ([][]string) {
 	newTweetStringsArray := make([][]string, len(tweets))
-	for i, _ := range tweets {
-		tweetStringsArray[i] = make([]string, 3)
-		tweetStringsArray[i][0] = tweet.IdStr
-		tweetStringsArray[i][1] = tweet.Text
-		tweetStringsArray[i][2] = tweet.CreatedAt
+	for i, tweet := range tweets {
+		newTweetStringsArray[i] = []string{tweet.IdStr, tweet.Text, tweet.CreatedAt}
 	}
 
 	return newTweetStringsArray
